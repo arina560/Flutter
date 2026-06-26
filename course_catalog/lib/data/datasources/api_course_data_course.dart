5086b021036b28ba8a5a0cac54d04a835957c245
Arina <petrozickaaarina@gmail.com>
2026-06-26 15:45:13 +0300

    added using of dio

--- course_catalog/lib/data/datasources/api_course_data_course.dart
+++ course_catalog/lib/data/datasources/api_course_data_course.dart
@@ -1,48 +1,42 @@
-import 'dart:convert';
-import 'dart:io';
+import 'package:dio/dio.dart';
 import 'package:course_catalog/data/datasources/course_data_source.dart';
 import '../models/course_dto.dart';
 
 class ApiCourseDataCourse implements CourseDataSourse{
   static const _baseUrl = 'https://6a3bdbbce4a07f202e160ec3.mockapi.io/api/v1';
-  final HttpClient _client;
-  ApiCourseDataCourse({HttpClient? client}) : _client = client ?? HttpClient();
+  final Dio _dio;
+  ApiCourseDataCourse({Dio? dio}) : _dio = dio ?? Dio(BaseOptions(baseUrl: _baseUrl));
 
   @override
   Future<List<CourseDto>> getCourses() async {
-    final uri = Uri.parse('$_baseUrl/courses');
-    final json = await _getJson(uri) as List<dynamic>;
-    return json
-        .cast<Map<String, dynamic>>()
-        .map(CourseDto.fromJson)
-        .toList();
+    try {
+      final response = await _dio.get('/courses');
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonList = response.data; 
+        return jsonList.cast<Map<String, dynamic>>().map(CourseDto.fromJson).toList();
+      } else {
+        throw Exception('Не удалось загрузить курсы: ${response.statusCode}');
+      }
+    } on DioException catch (e) {
+      throw Exception('Ошибка сети: ${e.message}');
+    }
   }
- 
   @override
   Future<CourseDto?> getCourseById(String id) async {
-    final uri = Uri.parse('$_baseUrl/courses/$id');
     try {
-      final json = await _getJson(uri) as Map<String, dynamic>;
-      return CourseDto.fromJson(json);
-    } on HttpException catch (_) {
-      return null;
+      final response = await _dio.get('/courses/$id');
+      if (response.statusCode == 200) {
+        return CourseDto.fromJson(response.data as Map<String, dynamic>);
+      } else if (response.statusCode == 404) {
+        return null;
+      } else {
+        throw Exception('Не удалось загрузить курсы: ${response.statusCode}');
+      }
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 404) {
+        return null;
+      }
+      throw Exception('Ошибка сети: ${e.message}');
     }
-  }
-
-  Future<dynamic> _getJson(Uri uri) async {
-    final request = await _client.getUrl(uri);
-    final response = await request.close();
- 
-    if (response.statusCode == 404) {
-      throw HttpException('Not found: $uri');
-    }
-    if (response.statusCode != 200) {
-      throw HttpException(
-        'HTTP ${response.statusCode} for $uri',
-      );
-    }
- 
-    final body = await response.transform(utf8.decoder).join();
-    return jsonDecode(body);
   }
 }
